838224644353a272aa1d5ae3b9d2c8d62934e33e
Grzegorz Antoniak <ga@anadoxin.org>
2018-11-01 15:41:38 +0100

    initial support for bzip2 in zipx format

--- libarchive/archive_read_support_format_zip.c
+++ libarchive/archive_read_support_format_zip.c
@@ -158,8 +158,6 @@ struct trad_enc_ctx {
 /**/
 #define MAX_DERIVED_KEY_BUF_SIZE	(AES_MAX_KEY_SIZE * 2 + 2)
 
-#define ZIPX_MIN(a, b) (((a) > (b)) ? (b) : (a))
-
 struct zip {
 	/* Structural information about the archive. */
 	struct archive_string	format_name;
@@ -1877,6 +1875,81 @@ zipx_bzip2_init(struct archive_read *a, struct zip *zip)
 
 static int
 zip_read_data_zipx_bzip2(struct archive_read *a, const void **buff,
+    size_t *size, int64_t *offset)
+{
+	struct zip *zip = (struct zip *)(a->format->data);
+	ssize_t bytes_avail = 0, in_bytes, to_consume;
+	const void *compressed_buff;
+	int r;
+
+	(void) offset; /* UNUSED */
+
+	LOG("read bzip2");
+
+	if(!zip->decompress_init) {
+		r = zipx_bzip2_init(a, zip);
+		if(r != ARCHIVE_OK)
+			return r;
+	}
+
+	compressed_buff = __archive_read_ahead(a, 1, &bytes_avail);
+	if(bytes_avail < 0) {
+		archive_set_error(&a->archive, ARCHIVE_ERRNO_FILE_FORMAT,
+			"Truncated ZIPx file body");
+		return (ARCHIVE_FATAL);
+	}
+
+	in_bytes = zipmin(zip->entry_bytes_remaining, bytes_avail);
+
+	zip->bzstream.next_in = (char*)(uintptr_t) compressed_buff;
+	zip->bzstream.avail_in = in_bytes;
+	zip->bzstream.total_in_hi32 = 0;
+	zip->bzstream.total_in_lo32 = 0;
+	zip->bzstream.next_out = (char*) zip->uncompressed_buffer;
+	zip->bzstream.avail_out = zip->uncompressed_buffer_size;
+	zip->bzstream.total_out_hi32 = 0;
+	zip->bzstream.total_out_lo32 = 0;
+
+	r = BZ2_bzDecompress(&zip->bzstream);
+	switch(r) {
+		case BZ_STREAM_END:
+			switch(BZ2_bzDecompressEnd(&zip->bzstream)) {
+				case BZ_OK:
+					break;
+				default:
+				archive_set_error(&a->archive, ARCHIVE_ERRNO_MISC,
+					"Failed to clean up bzip2 decompressor");
+				return ARCHIVE_FATAL;
+			}
+
+			break;
+		case BZ_OK:
+			break;
+		default:
+			archive_set_error(&a->archive, ARCHIVE_ERRNO_MISC,
+				"ZIPx BZIP2 decompression failed");
+			return ARCHIVE_FATAL;
+	}
+
+	to_consume = zip->bzstream.total_in_lo32;
+	__archive_read_consume(a, to_consume);
+
+	zip->entry_bytes_remaining -= to_consume;
+	zip->entry_compressed_bytes_read += to_consume;
+	zip->entry_uncompressed_bytes_read += zip->bzstream.total_out_lo32;
+
+	*size = zip->bzstream.total_out_lo32;
+	*buff = zip->uncompressed_buffer;
+
+	r = consume_optional_marker(a, zip);
+	if(r != ARCHIVE_OK)
+		return r;
+
+	return ARCHIVE_OK;
+}
+
+#endif
+
     size_t *size, int64_t *offset)
 {
 	struct zip *zip = (struct zip *)(a->format->data);
